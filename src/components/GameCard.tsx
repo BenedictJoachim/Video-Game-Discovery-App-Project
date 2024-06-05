e204f2882362d1e93b0aef6b84abe86b07f13333
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-06-05 09:42:17 +0000

    Rafactoring duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,11 +10,11 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
     return (
-        <Card borderRadius="10px" overflow="hidden">
+        <Card>
             <Image src={getCroppedImageUrl(game.background_image)} />
             <CardBody>
                 <Heading fontSize="2xl">{game.name}</Heading>
-                <HStack justifyContent="space-between">
+                <HStack justifyContent="space-between" flexWrap="wrap">
                     <PlatformiconList platforms={game.parent_platforms.map(p => p.platform)} />
                     <CriticScore score={game.metacritic} />
                 </HStack>

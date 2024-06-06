506b59627f96cf65d5b2a718f68c71005a67d35f
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-06-06 15:50:09 +0000

    Selecting platform on teh menu initial test

--- src/PlatformSelector.tsx
+++ src/PlatformSelector.tsx
@@ -1,8 +1,13 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react"
 import { BsChevronDown } from "react-icons/bs"
 import usePlatform from "./hooks/usePlatforms"
+import { Platform } from "./hooks/useGames"
 
-const PlatformSelector = () => {
+interface Props {
+    onSelectPlatform: (platform: Platform) => void;
+}
+
+const PlatformSelector = ({ onSelectPlatform }: Props) => {
     const {data, error} = usePlatform();
 
     if(error) return null;
@@ -15,7 +20,13 @@ const PlatformSelector = () => {
                 Platform
             </MenuButton>
             <MenuList>
-                {data.map(platform => <MenuItem key={platform.id}>{platform.name}</MenuItem> )}
+                {data.map(platform => 
+                    <MenuItem 
+                        key={platform.id}
+                        onClick={() => onSelectPlatform(platform)}
+                    >
+                        {platform.name}
+                    </MenuItem> )}
             </MenuList>
         </Menu>
     )

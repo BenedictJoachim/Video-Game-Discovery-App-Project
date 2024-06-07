a1f677589c4d38c7563fd2fdc2bbad84b5cebf3d
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-06-07 10:56:08 +0000

    Implimenting searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,15 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
+interface Props {
+    onSearch: (searchText: string) => void;
+}
 
-const NavBAr = () => {
+const NavBAr = ({ onSearch }: Props) => {
     return (
         <HStack padding="10px">
             <Image src={logo} boxSize="60px" />
-            <SearchInput />
+            <SearchInput onSearch={onSearch} />
             <ColorModeSwitch />
         </HStack>
     )
